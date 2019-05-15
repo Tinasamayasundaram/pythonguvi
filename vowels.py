543017d69fb9a8042d693dd1d983e3a6e197dd51
Tinasamayasundaram <50619385+Tinasamayasundaram@users.noreply.github.com>
2019-05-15 06:09:49 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,5 +1,5 @@
 ch=input()
-if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+if ch in ['a','e','i','o','u','A','E','I','O','U'];
     print("Vowel")
 else:
     print("Consonant")
